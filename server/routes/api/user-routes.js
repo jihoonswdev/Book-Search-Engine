38bcd22b2c8e0a43cce4d7de959ac524b9a13fc1
Jason Park <jihoonswdev@gmail.com>
2021-10-11 14:29:24 -0400

    update

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -12,12 +12,17 @@ const { authMiddleware } = require('../../utils/auth');
 
 // put authMiddleware anywhere we need to send a token for verification of user
 // url endpoint: /api/Users
-router.route('/').post(createUser).put(authMiddleware, saveBook);
+router.route('/')
+.post(createUser)
+.put(authMiddleware, saveBook);
 
-router.route('/login').post(login);
+router.route('/login')
+.post(login);
 
-router.route('/me').get(authMiddleware, getSingleUser);
+router.route('/me')
+.get(authMiddleware, getSingleUser);
 
-router.route('/books/:bookId').delete(authMiddleware, deleteBook);
+router.route('/books/:bookId')
+.delete(authMiddleware, deleteBook);
 
 module.exports = router;
