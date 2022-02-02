31a3b3e560e600c9ab48ff593182a76d3d26ead9
Jason Park <jihoonswdev@gmail.com>
2022-02-02 15:43:44 -0500

    fixing bugs

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,9 +1,8 @@
 // see SignupForm.js for comments
 import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { LOGIN_USER } from '../utils/mutations';
 import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 // import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
